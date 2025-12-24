ccbde0690930b58cd757b6792b16ab141f59c501
async <719939143@qq.com>
2025-12-24 13:59:30 +0800

    初始化项目

---
+++ src/test/java/com/newcoder/forums/ForumsApplicationTests.java
@@ -0,0 +1,12 @@
+package com.newcoder.forums;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+class ForumsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
